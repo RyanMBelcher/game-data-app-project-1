65ffcf9e572034d4bf03dd415dcb2315450fef7f
Joshua Carter <116673724+JoshuaCarter99@users.noreply.github.com>
2022-11-29 13:54:58 -0500

    ACCEPT
    ACCEPT

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,26 +5,30 @@ var rpg = document.getElementById('rpg');
 var platformer = document.getElementById('platformer');
 var simulation = document.getElementById('simulation');
 var sports = document.getElementById('sports');
+var any = document.getElementById('all');
 var platform = document.getElementById('platform');
 var playstation = document.getElementById('playstation');
 var xbox = document.getElementById('xbox');
 var pc = document.getElementById('pc');
 var nintendo = document.getElementById('nintendo');
+var all = document.getElementById('alltwo');
 var gotd = document.getElementById('game-of-the-day');
 var mostPlayed = document.getElementById('most-played');
 var userRated = document.getElementById('user-rated');
-var actionLink = '&genres=action'
-var adventureLink = '&genres=adventure'
-var rpgLink = '&genres=role-playing-games-rpg'
-var simulationLink = '&genres=simulation'
-var sportsLink = '&genres=sports'
-var platformerLink = '&genres=platformer'
-var strategyLink = '&genres=strategy'
-var playstationLink = '&parent_platforms=2'
-var pcLink = '&parent_platforms=1'
-var xboxLink = '&parent_platforms=3'
-var nintendoLink = '&parent_platforms=7'
-var starterLink = 'https://api.rawg.io/api/games?key=7fe5813e90774b17a77f1b43d96e25df&page_size=1'
+var generateBtn = document.getElementById('generate')
+var actionLink = '&genres=action';
+var adventureLink = '&genres=adventure';
+var rpgLink = '&genres=role-playing-games-rpg';
+var simulationLink = '&genres=simulation';
+var sportsLink = '&genres=sports';
+var platformerLink = '&genres=platformer';
+var strategyLink = '&genres=strategy';
+var playstationLink = '&parent_platforms=2';
+var pcLink = '&parent_platforms=1';
+var xboxLink = '&parent_platforms=3';
+var nintendoLink = '&parent_platforms=7';
+var starterLink = 'https://api.rawg.io/api/games?key=7fe5813e90774b17a77f1b43d96e25df&page_size=1';
+
 var firstkey = 'key=7fe5813e90774b17a77f1b43d96e25df';
 var secondkey = 'f137cf1f3c1fd662bd0326d342bf09b81662bb59';
 
@@ -42,7 +46,7 @@ function getTen(data) {
             gamesList.appendChild(listItem)
         }
     })
-}
+};
 
 function getTenMore(data) {
     fetch('https://api.rawg.io/api/games?key=7fe5813e90774b17a77f1b43d96e25df&page_size=10&dates=2022-01-01,2022-11-30&metacritic=88,100&ordering=-metacritic')
@@ -58,20 +62,63 @@ function getTenMore(data) {
             gameslistTwo.appendChild(listitemTwo)
         }
     })
-}
-
-function getOne(data) {
-    fetch('https://api.rawg.io/api/games?key=7fe5813e90774b17a77f1b43d96e25df&page_size=1')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data)
-    })
-}
+};
 
 function randomGame() {
-    fetch('https://api.rawg.io/api/games?key=7fe5813e90774b17a77f1b43d96e25df')
+    // gameplatformGenerator();
+    var platformpick = document.getElementById('platformpick');
+    console.log(platformpick.value);
+    // platform variables
+    var playstationLink = '&parent_platforms=2';
+    var pcLink = '&parent_platforms=1';
+    var xboxLink = '&parent_platforms=3';
+    var nintendoLink = '&parent_platforms=7';
+    // gamegenreGenerator();
+    var genrepick = document.getElementById('genrepick');
+    console.log(genrepick.value)
+    // genre variables
+    var actionLink = '&genres=action';
+    var adventureLink = '&genres=adventure';
+    var rpgLink = '&genres=role-playing-games-rpg';
+    var simulationLink = '&genres=simulation';
+    var sportsLink = '&genres=sports';
+    var platformerLink = '&genres=platformer';
+    var strategyLink = '&genres=strategy';
+    var starterLink = 'https://api.rawg.io/api/games?key=7fe5813e90774b17a77f1b43d96e25df';
+
+    if (platformpick.value === 'playstation') {
+        starterLink = starterLink + playstationLink
+    } else if (platformpick.value === 'pc') {
+        starterLink = starterLink + pcLink
+    } else if (platformpick.value === 'xbox') {
+        starterLink = starterLink + xboxLink
+    } else if (platformpick.value === 'nintendo') {
+        starterLink = starterLink + nintendoLink
+    } else {
+        starterLink = starterLink
+    }
+
+    if (genrepick.value === 'action') {
+        starterLink = starterLink + actionLink
+    } else if (genrepick.value === 'adventure') {
+        starterLink = starterLink + adventureLink
+    } else if (genrepick.value === 'rpg') {
+        starterLink = starterLink + rpgLink
+    } else if (genrepick.value === 'platformer') {
+        starterLink = starterLink + platformerLink
+    } else if (genrepick.value === 'strategy') {
+        starterLink = starterLink + strategyLink
+    } else if (genrepick.value === 'sports') {
+        starterLink = starterLink + sportsLink
+    } else if (genrepick.value === 'simulation') {
+        starterLink = starterLink + simulationLink
+    } else {
+        starterLink = starterLink
+    }
+    
+    console.log(starterLink)
+
+    fetch(starterLink)
     .then(function (response) {
         return response.json();
     })
@@ -80,63 +127,18 @@ function randomGame() {
     const random1 = array[(Math.floor(Math.random() * (array.length)))]
     console.log(random1)
     })
-}
+};
 
+console.log(getTen())
+console.log(getTenMore())
 console.log(randomGame())
 
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data)
-        })
-}
-function getOne(data) {
-    fetch('https://api.rawg.io/api/games?key=7fe5813e90774b17a77f1b43d96e25df&page_size=1')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data)
-        })
-}
-function randomGame() {
-    fetch('https://api.rawg.io/api/games?key=7fe5813e90774b17a77f1b43d96e25df')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            const array = data.results
-            const random1 = array[(Math.floor(Math.random() * (array.length)))]
-            console.log(random1)
-        })
-}
-console.log(randomGame())
+getTen();
+getTenMore();
+randomGame();
+generateBtn.addEventListener('click', randomGame);
 
 // REVIEWS
 // 'https://www.giantbomb.com/api/user_reviews/?api_key=f137cf1f3c1fd662bd0326d342bf09b81662bb59'
 // Pull parameter 'score' to reveal general user score
 // MATCH API PARAMETERS 'slug'='game' OR 'name'='game'. Parameter 'game' belongs to giantbomb, parameter 'slug'/'name' belongs to RAWG.
-
-// FILTER GENRE
-// 'https://api.rawg.io/api/games?key=7fe5813e90774b17a77f1b43d96e25df&genres=action'
-// 'https://api.rawg.io/api/games?key=7fe5813e90774b17a77f1b43d96e25df&genres=adventure'
-// 'https://api.rawg.io/api/games?key=7fe5813e90774b17a77f1b43d96e25df&genres=role-playing-games-rpg'
-// 'https://api.rawg.io/api/games?key=7fe5813e90774b17a77f1b43d96e25df&genres=platformer'
-// 'https://api.rawg.io/api/games?key=7fe5813e90774b17a77f1b43d96e25df&genres=simulation'
-// 'https://api.rawg.io/api/games?key=7fe5813e90774b17a77f1b43d96e25df&genres=strategy'
-// 'https://api.rawg.io/api/games?key=7fe5813e90774b17a77f1b43d96e25df&genres=sports'
-
-// FILTER PLATFORM
-// 'https://api.rawg.io/api/games?key=7fe5813e90774b17a77f1b43d96e25df&parent_platforms=1'
-// PC
-// 'https://api.rawg.io/api/games?key=7fe5813e90774b17a77f1b43d96e25df&parent_platforms=2'
-// Playstation
-// 'https://api.rawg.io/api/games?key=7fe5813e90774b17a77f1b43d96e25df&parent_platforms=3'
-// Xbox
-// 'https://api.rawg.io/api/games?key=7fe5813e90774b17a77f1b43d96e25df&parent_platforms=7'
-// Nintendo
-
-getTen();
-getTenMore();
-getOne();
